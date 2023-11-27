077036860d5158f5968fb5ffc3ef001a3e8658be
Eurese Antonio Bustamante <eurese.bustamante@orangeandbronze.com>
2023-11-27 20:03:07 +0800

    Updated Backend File

--- backend/teknoplat_server/criteria/api/serializers.py
+++ backend/teknoplat_server/criteria/api/serializers.py
@@ -9,13 +9,18 @@ class CriteriaSerializer(serializers.ModelSerializer):
 class MeetingCriteriaSerializer(serializers.ModelSerializer):
     criteria = serializers.SerializerMethodField()
     meeting = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
     class Meta:
         model = MeetingCriteria
-        fields = ('meeting', 'criteria', 'weight')
+        fields = ('id', 'meeting', 'criteria', 'description', 'weight')
 
     def get_criteria(self, obj):
         return obj.criteria.name
     
     def get_meeting(self, obj):
         return obj.meeting.name
+    
+    def get_description(self, obj):
+        return obj.criteria.description
+    
