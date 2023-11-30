e3ccb335a63f2c1dff03ae6e1056f9590f308589
Albrecht Christian Lanojan <albrecht.lanojan0210@gmail.com>
2023-12-01 00:37:38 +0800

    Remove fetching team and members

--- backend/teknoplat_server/pitches/api/urls.py
+++ backend/teknoplat_server/pitches/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PitchViewSet
+from .views import PitchViewSet, AccountPitchAPIView
 
 router = DefaultRouter()
 router.register(r'pitches', PitchViewSet, basename='pitch')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('pitches/<int:pk>/get_team/', PitchViewSet.as_view({'get': 'get_team'}), name='get-team'),
-    path('pitches/<int:pk>/get_team_members/', PitchViewSet.as_view({'get': 'get_team_members'}), name='get-team-members'),
-    
+    # path('pitches/<int:pk>/get_team/', PitchViewSet.as_view({'get': 'get_team'}), name='get-team'),
+    # path('pitches/<int:pk>/get_team_members/', PitchViewSet.as_view({'get': 'get_team_members'}), name='get-team-members'),
+    path('account/pitch/', AccountPitchAPIView.as_view(), name='account-pitch')
 ]
--- backend/teknoplat_server/pitches/api/views.py
+++ backend/teknoplat_server/pitches/api/views.py
@@ -1,6 +1,5 @@
-import requests
-from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import action
+# import requests
+from rest_framework import generics, permissions, status, viewsets
 from rest_framework.response import Response
 from ..models import Pitch
 from .serializers import PitchSerializer
@@ -11,53 +10,49 @@ class PitchViewSet(viewsets.ModelViewSet):
     serializer_class = PitchSerializer
     permission_classes = (permissions.IsAuthenticated, IsTeacherUserOrReadOnly, )
 
-    def get_auth_headers(self):
-        authorization_header = self.request.META.get('HTTP_AUTHORIZATION', None)
-        _, token = authorization_header.split()
-        return {'Authorization': f"Bearer {token}"}
+    # def get_auth_headers(self):
+    #     authorization_header = self.request.META.get('HTTP_AUTHORIZATION', None)
+    #     _, token = authorization_header.split()
+    #     return {'Authorization': f"Bearer {token}"}
 
-    def has_error_response(self, status_code):
-        error_response_map = [500, 400, 401, 403, 404]
-        if status_code in error_response_map:
-            return True
-        return False
+    # def has_error_response(self, status_code):
+    #     error_response_map = [500, 400, 401, 403, 404]
+    #     if status_code in error_response_map:
+    #         return True
+    #     return False
 
-    def fetch_team(self, team_id):
-        headers = self.get_auth_headers()
-        response = requests.get(f'http://localhost:8080/api/teams/{team_id}/', headers=headers)
-        return response
+    # def fetch_team(self, team_id):
+    #     headers = self.get_auth_headers()
+    #     response = requests.get(f'http://localhost:8080/api/teams/{team_id}/', headers=headers)
+    #     return response
     
-    def fetch_team_members(self, team_id):
-        headers = self.get_auth_headers()
-        response = requests.get(f'http://localhost:8080/api/teams/{team_id}/get_team_members/', headers=headers)
-        return response
-    
-    def create(self, request, *args, **kwargs):
-        team_id = request.data.get('team')
-        team = self.fetch_team(team_id)
+    # def create(self, request, *args, **kwargs):
+    #     team_id = request.data.get('team')
+    #     team = self.fetch_team(team_id)
 
-        if self.has_error_response(team.status_code):
-            return Response({'error': 'Failed to fetch teams.'}, status=status.HTTP_400_BAD_REQUEST)
+    #     if self.has_error_response(team.status_code):
+    #         return Response({'error': 'Failed to fetch teams.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        pitch_serializer = self.get_serializer(data=request.data)
+    #     pitch_serializer = self.get_serializer(data=request.data)
 
-        if pitch_serializer.is_valid():
-            pitch_serializer.save()
-            return Response(pitch_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(pitch_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    @action(detail=True, methods=['get'])
-    def get_team(self, request, pk=None):
-        pitch = self.get_object()
-        team = self.fetch_team(pitch.team)
-        
-        return Response(team.json(), status=status.HTTP_200_OK)
-    
-    @action(detail=True, methods=['get'])
-    def get_team_members(self, request, pk=None):
-        pitch = self.get_object()
-        members = self.fetch_team_members(pitch.team)
-        
-        return Response(members.json(), status=status.HTTP_200_OK)
+    #     if pitch_serializer.is_valid():
+    #         pitch_serializer.save()
+    #         return Response(pitch_serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(pitch_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    
+class AccountPitchAPIView(generics.ListCreateAPIView):
+    serializer_class = PitchSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_queryset(self):
+        team_param = self.request.query_params.get('team', None)
+
+        if (team_param):
+            try:
+                queryset = Pitch.objects.filter(team=team_param)
+                if not queryset.exists():
+                    raise Pitch.DoesNotExist
+                return queryset
+            except Pitch.DoesNotExist:
+                return Response({'error', 'Pitch does not exists.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error', 'Add parameter team.'}, status=status.HTTP_400_BAD_REQUEST)
