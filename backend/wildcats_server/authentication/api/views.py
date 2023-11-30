35fb908440455ca2e2f4baf0ea9e5952fc85d4f1
Albrecht Christian Lanojan <albrecht.lanojan0210@gmail.com>
2023-12-01 00:34:47 +0800

    Remove Video Authenticate

--- backend/wildcats_server/authentication/api/views.py
+++ backend/wildcats_server/authentication/api/views.py
@@ -15,19 +15,3 @@ class PublicKeyAPIView(views.APIView):
     
 class WildcatTokenObtainPairView(TokenObtainPairView):
     serializer_class = WildcatTokenObtainPairSerializer
-    
-class WildcatGenerateVideoToken(views.APIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    VIDEOSDK_API_KEY  = "fad76f00-2b26-41ae-ab29-698c9d6ca4a1"
-    VIDEOSDK_SECRET_KEY = "16af5ce994c657c13e80ec4aba564949f11d1ea267b941973187ecc0766a0bb8"
-    
-    def get(self, request, *args, **kwargs):
-        expiration_in_seconds = 600
-        expiration = datetime.datetime.now() + datetime.timedelta(seconds=expiration_in_seconds)
-        token = jwt.encode(payload={
-            'exp': expiration,
-            'apikey': self.VIDEOSDK_API_KEY,
-            'permissions': ["allow_join", "allow_mod"],
-        }, key=self.VIDEOSDK_SECRET_KEY, algorithm="HS256")
-
-        return Response(token, status=status.HTTP_200_OK)
