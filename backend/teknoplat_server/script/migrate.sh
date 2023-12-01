d63dce8a2ecd918e43799b03c61940f7882c8abf
Albrecht Christian Lanojan <albrecht.lanojan0210@gmail.com>
2023-12-01 11:57:19 +0800

    Add Models to Admin Site

--- backend/teknoplat_server/script/migrate.sh
+++ backend/teknoplat_server/script/migrate.sh
@@ -6,7 +6,9 @@ python manage.py makemigrations meetings
 python manage.py makemigrations comments
 python manage.py makemigrations chats
 python manage.py makemigrations criteria
-python manage.py makemigrations evaluations
 python manage.py makemigrations feedbacks
+python manage.py makemigrations ratings
+python manage.py makemigrations remarks
+python manage.py makemigrations video
 
 python manage.py migrate
